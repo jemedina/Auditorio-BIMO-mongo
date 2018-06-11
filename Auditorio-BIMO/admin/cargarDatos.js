11551b3e6579c99f793eec2217aa99753289ba87
Eduardo <jemedina.96@gmail.com>
2018-06-11 15:30:55 -0500

    Fix

--- Auditorio-BIMO/admin/cargarDatos.js
+++ Auditorio-BIMO/admin/cargarDatos.js
@@ -130,8 +130,8 @@
                         $("#inputDesc").val(promo.descripcion);
                         var fIniArr = promo.fecha_inicio.split("/");
                         var fFinArr = promo.fecha_fin.split("/");
-                        $("#inputFinicio").val(fIniArr[2] +"-"+ fIniArr[1] +"-"+ fIniArr[0]);
-                        $("#inputf_final").val(fFinArr[2] +"-"+ fFinArr[1] +"-"+ fFinArr[0]);
+                        $("#inputFinicio").val(fIniArr[2] +"-"+ fIniArr[0] +"-"+ fIniArr[1]);
+                        $("#inputf_final").val(fFinArr[2] +"-"+ fFinArr[0] +"-"+ fFinArr[1]);
                         $("#inputDescuento").val(promo.descuento);
                     }
                     updateFields();
@@ -151,6 +151,7 @@
                 Promociones.updatepromo($("#selectPromos").val(), $("#inputDesc").val(), $("#inputFinicio").val(), $("#inputf_final").val(),$("#inputDescuento").val(), function(resp){
                     if(resp == true){
                         alert("Promocion agregada actualizada!");
+                        window.location.reload();
                     }
                 });
             });
@@ -159,44 +160,4 @@
         } 
     });
 
-    
-
-    function cargaIDFunciones(ids) {
-        for (var a in ids) {
-            option = document.createElement("option");
-            option.value = 1234;//ids.value
-            option.text = "1234";//ids.text
-            $('#inputID').append(option)
-        }
-    }
-
-    function cargarDatosFunciones(fecha, horario) {
-        var fecha = '2018-05-14';// Formato de AAAA-MM-DD
-        var horario = '10:20'//Formato de 24 Hrs
-        $('#inputFecha').val(fecha);
-        $('#inputHorario').val(horario);   
-    }
-
-
-    function cargaNoPromos(promos) {
-        $("#selectPromos").empty();
-        for (var a in promos) {
-            option = document.createElement("option");
-            option.value = 1234;//promos.value
-            option.text = "1234";//promos.text
-            $('#selectPromos').append(option)
-        }
-    }
-
-
-    function cargarDatosPromos(descripcion, f_inicio, f_final, descuento) {
-        var descripcion= "Hola mundo como estan";
-        var f_inicio = '2018-05-14';// Formato de AAAA-MM-DD
-        var f_final = '2018-05-18';// Formato de AAAA-MM-DD
-        var descuento = 20
-        $('#inputDesc').val(descripcion);
-        $('#inputFinicio').val(f_inicio);
-        $('#inputf_final').val(f_final);
-        $('#inputDescuento').val(descuento);
-    }
 })();
